26d717ea8d87e967fdd308e51f07bfef26da42ae
spoonvash <spoonvash@gmail.com>
2026-01-09 17:45:17 +0000

    Fix version constants

--- oversee-helpdesk/oversee-helpdesk/oversee-helpdesk.php
+++ oversee-helpdesk/oversee-helpdesk/oversee-helpdesk.php
@@ -30,7 +30,7 @@ if (version_compare(PHP_VERSION, '7.4.0', '<')) {
 }
 
 // Plugin constants
-define('OVERSEE_VERSION', '2.1.0');
+define('OVERSEE_VERSION', '2.1.3');
 define('OVERSEE_PLUGIN_FILE', __FILE__);
 define('OVERSEE_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('OVERSEE_PLUGIN_URL', plugin_dir_url(__FILE__));
@@ -894,4 +894,4 @@ class Oversee_Helpdesk_Updater {
 }
 
 // Initialize updater
-new Oversee_Helpdesk_Updater(__FILE__, '2.1.1');
+new Oversee_Helpdesk_Updater(__FILE__, '2.1.3');
